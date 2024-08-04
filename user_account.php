1068165e85f6ba09027cb471aec3297efb0ab06d
Berkay Samet Saatçi <115094387+bsaatci16@users.noreply.github.com>
2024-08-04 18:59:39 +0300

    Create user_account.php

---
+++ user_account.php
@@ -0,0 +1,16 @@
+<?php include 'includes/header.php'; ?>
+<div class="container mt-4">
+    <h1>Hesabım</h1>
+    <form>
+        <div class="form-group">
+            <label for="username">Kullanıcı Adı</label>
+            <input type="text" class="form-control" id="username">
+        </div>
+        <div class="form-group">
+            <label for="password">Şifre</label>
+            <input type="password" class="form-control" id="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Giriş Yap</button>
+    </form>
+</div>
+<?php include 'includes/footer.php'; ?>

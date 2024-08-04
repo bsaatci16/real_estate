9307ee969bd941da5a4a3d6f47a83a92190e7656
Berkay Samet Saatçi <115094387+bsaatci16@users.noreply.github.com>
2024-08-04 18:58:50 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+<div class="container mt-4">
+    <h1>Hakkımızda</h1>
+    <p>Şirket bilgisi burada yer alacak.</p>
+</div>
+<?php include 'includes/footer.php'; ?>
